80930b78b9508010f2d083551a906f3d088787af
Jerome (Chengwu) Zheng <czheng@hydrox23.internal.synopsys.com>
2019-03-06 14:36:35 +0800

    complete version of 101.symmetric-tree.cpp

--- 101.symmetric-tree.cpp
+++ 101.symmetric-tree.cpp
@@ -6,39 +6,54 @@
      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  };
 
-#include <vector>
+//#include <vector>
 bool visit_left_first(TreeNode* node, std::vector<int> &valueArray, TreeNode* root, bool &is_left_part);
 class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
-       std:: vector<int> valueArray;
-       bool is_left_part = true;
-       return (visit_left_first(root, valueArray, root, is_left_part) && valueArray.empty());
+       // std:: vector<int> valueArray;
+       // bool is_left_part = true;
+       // return (visit_left_first(root, valueArray, root, is_left_part) && valueArray.empty());
+       if(root)
+          return identifier(root->left, root->right);
+       else
+          return true;
 
         
     }
+
+   bool identifier(TreeNode *lnode, TreeNode *rnode){
+      if(lnode == NULL && rnode == NULL){
+         return true;
+      } else if( lnode == NULL || rnode == NULL){
+         return false;
+      }
+
+      if(lnode->val !=rnode->val) return false;
+      return (identifier(lnode->left, rnode->right) && identifier(lnode->right, rnode->left));
+   }
 };
 
-bool visit_left_first(TreeNode* node, std::vector<int> &valueArray, TreeNode* root, bool &is_left_part){
-   if(node == NULL) return true;
-   bool result2 = true;
-   bool result1 = visit_left_first(node->left, valueArray, root, is_left_part);
-   if(node != root){
-      if(is_left_part)
-         valueArray.push_back(node->val);
-      else {
-         if(!valueArray.empty()){
-            int value = valueArray.back();
-            valueArray.pop_back();
-            if(value != node->val)
-               result2 =  false;
-         } else {
-            result2 = false;
-         }
-      }
-   } else {
-      is_left_part = false;
-   }
-   bool result3 = visit_left_first(node->right, valueArray, root, is_left_part);
-   return (result1 && result2 && result3);
-}
+// bool visit_left_first(TreeNode* node, std::vector<int> &valueArray, TreeNode* root, bool &is_left_part){
+//    if(node == NULL) return true;
+//    bool result2 = true;
+//    bool result1 = visit_left_first(node->left, valueArray, root, is_left_part);
+//    if(node != root){
+//       if(is_left_part)
+//          valueArray.push_back(node->val);
+//       else {
+//          if(!valueArray.empty()){
+//             int value = valueArray.back();
+//             valueArray.pop_back();
+//             if(value != node->val)
+//                result2 =  false;
+//          } else {
+//             result2 = false;
+//          }
+//       }
+//    } else {
+//       is_left_part = false;
+//    }
+//    bool result3 = visit_left_first(node->right, valueArray, root, is_left_part);
+//    return (result1 && result2 && result3);
+// }
